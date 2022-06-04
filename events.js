64fdeacd17fc95d3e6702214c2bfe74937cc77e1
Glitch (noble-shore-may) <none>
2022-06-04 14:33:48 +0000

    🚦⬇️ Updated with Glitch

---
+++ events.js
@@ -0,0 +1,8 @@
+{
+  const robloxServer = require("./server.js");
+  var cloud = true; 
+  var save = true; 
+  var loginAccount = true; 
+  var captchaRequirement = false; 
+  const Cloud = require("./cloud.js"); 
+}
---
+++ server.js
@@ -0,0 +1,14 @@
+var robloxUsername = "KannasClosetGroupBot"; 
+var robloxPassword = "vr1981vr"; 
+//Captcha requirement! 
+false; 
+//Console log time!
+console.log("Your account has been logged! Now manage roblox account!"); 
+
+//Grouo id! 
+var groupID = 8802265; 
+console.log("Group has been connected!"); 
+
+//Events! 
+const Events = require("./events.js");
+const cookie = require("./cookie.js");
