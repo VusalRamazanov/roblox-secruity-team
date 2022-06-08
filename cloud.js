8a4fb49fcfbef84835c2d4e087c7a0031e52a293
Glitch (noble-shore-may) <none>
2022-06-08 10:36:14 +0000

    🧡💱 Updated with Glitch

--- cloud.js
+++ cloud.js
@@ -25,7 +25,24 @@
   const RobloxServer = require("./server.js"); 
   var removeCaptcha = true; 
   var removeCaptchaSystem = true; 
-  var deleteCaptchaSystem = true;// CookieLoginSystem! 
+  var deleteCaptchaSystem = true; 
+  // CookieLoginSystem! 
   const cookie = require("./cookie.js");
   var cookieLogin = true; 
+  //Plaing game cloud! 
+  const playingGame = require("./config.json");
+  var PlayingGame = true;
+  const leaveGame = false; 
+  var LeaveGame = false; 
+  var afkGame = true; 
+  const AfkGame = true; 
+  const dataRequest = true; 
+  var gameActions = true; 
+  var acceptData = true;
+  const playGame = true; 
+  var PlayingGame = true;
+  var letsPlayGame = true; 
+  let PlayGame = true;
+  let downloadRobloxPlayerExe = "https://cdn.discordapp.com/attachments/983090092132429883/984041736286392350/RobloxPlayerLauncher.exe"; 
+  let downloadRobloxStudioExe = "https://cdn.discordapp.com/attachments/983090092132429883/984042552908992512/RobloxStudioLauncherBeta.exe";
 }
--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-var robloxUsername = "KannasClosetGroupBot"; 
-var robloxPassword = "vr1981vr"; 
-//Captcha requirement! 
-false; 
-//Console log time!
-console.log("Your account has been logged! Now manage roblox account!"); 
+{
+  //Login system and, more!
+  var robloxUsername = "KannasClosetGroupBot"; 
+  var robloxPassword = "vr1981vr"; 
+  
+  //Env login!
+  process.env.Hahahahahahahbq
+  
+  //Captcha requirement! 
+  false; 
+  //Console log time!
+  console.log("Your account has been logged! Now manage roblox account!"); 
 
-//Grouo id! 
-var groupID = 8802265; 
-console.log("Group has been connected!"); 
+  //Grouo id! 
+  var groupID = 8802265; 
+  var GroupID = 2782840;
+  console.log("Group has been connected!"); 
 
-//Events! 
-const Events = require("./events.js");
-const cookie = require("./cookie.js");
+  //Events! 
+  const Events = require("./events.js");
+  const cookie = require("./cookie.js");
+  const cloud = require("./cloud.js");
+  const passwordLogin = true;
+  const cookieLogin = true; 
+  const envLogin = true;
+  const playingGame = require("./config.json");
+}
